533805a826c389cc212233be76b1b6660a4c75f5
nvclas <74192845+nvclas@users.noreply.github.com>
2022-05-17 21:42:15 +0200

    added language support for some languages

---
+++ src/main/java/de/theniclas/autogl/utils/ServerMessages.java
@@ -0,0 +1,17 @@
+package de.theniclas.autogl.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ServerMessages {
+
+	public static List<String> messages = Arrays.asList(
+		"§r§7Die Runde beginnt in §r§e5§r§7 Sekunden§r",
+		"§r§7The game starts in §r§e5§r§7 seconds§r",
+		"§r§7D'Rundi startet ih §r§e5§r§7 Sekunde§r",
+		"§r§7De Rundn startet in §r§e5§r§7 Sekundn§r",
+		"§r§7Oyun §r§e5§r§7 saniye içinde başlıyor§r",
+		"§r§7Игра начнется через §r§e5§r§7 секунд§r"
+	);
+	
+}
